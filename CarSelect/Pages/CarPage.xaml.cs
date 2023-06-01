dd3525492fc9fcece6cab0664cef7418bd84b3f7
gutufik <gutufik@mail.ru>
2023-06-01 10:27:40 +0300

    add car page

---
+++ CarSelect/Pages/CarPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using CarSelect.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarSelect.Pages
+{
+    /// <summary>
+    /// Interaction logic for CarPage.xaml
+    /// </summary>
+    public partial class CarPage : Page
+    {
+        public List<BodyType> BodyTypes { get; set; }
+        public List<Brand> Brands { get; set; }
+
+        public List<Model> Models { get; set; }
+
+        public CarPage(Car car)
+        {
+            InitializeComponent();
+            BodyTypes = DataAccess.GetBodyTypes();
+            Brands = DataAccess.GetBrands();
+            Models = DataAccess.GetModels();
+
+
+            DataContext = this;
+        }
+    }
+}
